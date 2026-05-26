4c91102e38df2a2734aa085378bcd24503772001
kuanlinyu <k025g1032@g.neec.ac.jp>
2026-05-26 12:51:42 +0900

    fix

--- main.cpp
+++ main.cpp
@@ -377,10 +377,10 @@ Matrix4x4 MakeOrthoGraphicMatrix(float left, float top, float right, float botto
 	Matrix4x4 result{};
 	result.m[0][0] = 2.0f / (right - left);
 	result.m[1][1] = 2.0f / (top - bottom);
-	result.m[2][2] = -2.0f / (farZ - nearZ);
+	result.m[2][2] = 1.0f / (farZ - nearZ);
 	result.m[3][0] = -(right + left) / (right - left);
 	result.m[3][1] = -(top + bottom) / (top - bottom);
-	result.m[3][2] = -(farZ + nearZ) / (farZ - nearZ);
+	result.m[3][2] = -(nearZ) / (farZ - nearZ);
 	result.m[3][3] = 1.0f;
 	return result;
 }
