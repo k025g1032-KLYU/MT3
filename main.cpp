af917d09aba06dd3b274d0c56bd5a9664e2392dd
kuanlinyu <k025g1032@g.neec.ac.jp>
2026-05-28 15:54:43 +0900

    fix

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <Novice.h>
 #define _USE_MATH_DEFINES 
-#include <math.h>
+#include <cmath>
 
 const char kWindowTitle[] = "GC1A_11_ヨ_カンリン_タイトル";
 
@@ -42,10 +42,10 @@ Matrix4x4 MakeRotationXMatrix(float radian) {
 	Matrix4x4 result{};
 
 	result.m[0][0] = 1.0f;
-	result.m[1][1] = cosf(radian);
-	result.m[1][2] = sinf(radian);
-	result.m[2][1] = -sinf(radian);
-	result.m[2][2] = cosf(radian);
+	result.m[1][1] = std::cos(radian);
+	result.m[1][2] = std::sinf(radian);
+	result.m[2][1] = -std::sinf(radian);
+	result.m[2][2] = std::cos(radian);
 	result.m[3][3] = 1.0f;
 
 	return result;
@@ -54,11 +54,11 @@ Matrix4x4 MakeRotationXMatrix(float radian) {
 Matrix4x4 MakeRotationYMatrix(float radian) {
 	Matrix4x4 result{};
 
-	result.m[0][0] = cosf(radian);
-	result.m[0][2] = -sinf(radian);
+	result.m[0][0] = std::cos(radian);
+	result.m[0][2] = -std::sinf(radian);
 	result.m[1][1] = 1.0f;
-	result.m[2][0] = sinf(radian);
-	result.m[2][2] = cosf(radian);
+	result.m[2][0] = std::sinf(radian);
+	result.m[2][2] = std::cos(radian);
 	result.m[3][3] = 1.0f;
 
 	return result;
@@ -67,10 +67,10 @@ Matrix4x4 MakeRotationYMatrix(float radian) {
 Matrix4x4 MakeRotationZMatrix(float radian) {
 	Matrix4x4 result{};
 
-	result.m[0][0] = cosf(radian);
-	result.m[0][1] = sinf(radian);
-	result.m[1][0] = -sinf(radian);
-	result.m[1][1] = cosf(radian);
+	result.m[0][0] = std::cos(radian);
+	result.m[0][1] = std::sinf(radian);
+	result.m[1][0] = -std::sinf(radian);
+	result.m[1][1] = std::cos(radian);
 	result.m[2][2] = 1.0f;
 	result.m[3][3] = 1.0f;
 
