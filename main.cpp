43407e08d8c9890dd4bc456212def3d526ec5b40
kuanlinyu <k025g1032@g.neec.ac.jp>
2026-06-30 11:29:20 +0900

    OK

--- main.cpp
+++ main.cpp
@@ -1010,14 +1010,14 @@ Vector3 operator+(const Vector3& v) { return v; }
 //複合代入演算子
 Vector3& operator+=(Vector3& v1, Vector3& v2) {
 	v1.x += v2.x;
-	v1.y += v2.x;
-	v1.z += v2.x;
+	v1.y += v2.y;
+	v1.z += v2.z;
 	return v1;
 }
 Vector3& operator-=(Vector3& v1, Vector3& v2) {
 	v1.x -= v2.x;
-	v1.y -= v2.x;
-	v1.z -= v2.x;
+	v1.y -= v2.y;
+	v1.z -= v2.z;
 	return v1;
 }
 Vector3& operator*=(Vector3& v, float s) {
